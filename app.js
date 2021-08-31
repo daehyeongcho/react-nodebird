6a40ad0f37561ac5feddc64f53d18311193924de
PC-102ILSEONG <dhcho1034@gmail.com>
2021-08-31 18:25:44 +0900

    게시글, 댓글 작성하고 credentials로 쿠키 공유하기

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ passportConfig() // passport 모듈
 app.use(
 	cors({
 		origin: 'http://localhost:3000',
-		credentials: true,
+		credentials: true, // 쿠키 허용
 	}),
 )
 app.use(express.json())
--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,61 @@
 const express = require('express')
+
+const { Post, Comment, Image, User } = require('../models')
+const { isLoggedIn } = require('./middlewares')
+
 const router = express.Router()
 
 /* POST /post/ */
-router.post('/', (req, res) => {
-	res.json({ id: 1, content: 'hello' })
+router.post('/', isLoggedIn, async (req, res, next) => {
+	try {
+		const post = await Post.create({
+			content: req.body.content,
+			UserEmail: req.user.email, // deserializeUser에서 req.user 만들어줌
+		})
+
+		/* 정보를 완성해서 돌려주기 */
+		const fullPost = await Post.findOne({
+			where: { id: post.id },
+			include: [
+				{
+					model: Image,
+				},
+				{
+					model: Comment,
+				},
+				{
+					model: User,
+				},
+			],
+		})
+		res.status(201).json(post)
+	} catch (err) {
+		console.log(error)
+		next(err)
+	}
+})
+
+/* POST /post/1/comment */
+router.post('/:postId/comment', isLoggedIn, async (req, res, next) => {
+	try {
+		/* 존재하지 않는 게시글에 댓글 작성하려고 하는 경우 */
+		const post = await Post.findOne({
+			where: { id: req.params.postId },
+		})
+		if (!post) {
+			return res.status(403).send('존재하지 않는 게시글입니다.')
+		}
+
+		const comment = await Comment.create({
+			content: req.body.content,
+			PostId: req.params.postId,
+			UserEmail: req.user.email, // deserializeUser에서 req.user 만들어줌
+		}) // comment 생성하고 DB에 저장
+		res.status(201).json(comment)
+	} catch (err) {
+		console.log(error)
+		next(err)
+	}
 })
 
 /* DELETE /posts/ */
